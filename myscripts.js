233f770ce481e3005968870e0507813a1c336a7b
nanintell <90757058+nanintell@users.noreply.github.com>
2021-12-12 15:22:38 +0700

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
 function goto(target){
-    window.location.href = location.protocol + '//' + location.host+"/Project/" + target;
+    window.location.href = location.protocol + '//' + location.host+"/CPE327-term-project-main/" + target;
   }
   
